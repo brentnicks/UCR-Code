653c7aeeac03c4ee2309d331a74bb987d0aa43ee
brentnicks <brent_nicks@msn.com>
2025-04-02 16:55:35 -0700

    CS 218 #1 C

--- CS 218/HW1-Challenge/C_Share_Candies.cpp
+++ CS 218/HW1-Challenge/C_Share_Candies.cpp
@@ -5,7 +5,42 @@
 
 using namespace std;
 
+bool isValid(int target, vector<pair<long long, long long>> pos) {
+    int n = pos.size();
 
+    int left = 0;
+    for (int i = 0; i < n; i++) {
+        while (left < n && pos[left].second >= target) {
+            left++;
+            if (left >= n) {
+                break;
+            }
+        }
+        if (pos[i].second > target) {
+            long long needed = target - pos[left].second;
+            long long toSpare = pos[i].second - target;
+            long long dist = pos[i].first - pos[left].first;
+            if (toSpare > dist) {
+                pos[left].second += min(needed, toSpare - dist);
+                pos[i].second -= min(needed + dist, toSpare);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << pos[i].second << " ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        if (pos[i].second < target) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -14,23 +49,25 @@ int main() {
     int n;
     cin >> n;
 
-    vector<long long> pos;
+    vector<pair<long long, long long>> pos;
     long long candy = 0;
 
     for (int i = 0; i < n; i++) {
         long long p, c;
         cin >> p >> c;
-        pos.push_back(p);
+        pos.push_back({p, c});
         candy += c;
     }
 
-    long long dist = 0;
+    // long long dist = 0;
 
-    for (int i = 0; i < n - 1; i++) {
-        dist += abs(pos[i + 1] - pos[i]);
-    }
+    // for (int i = 0; i < n - 1; i++) {
+    //     dist += abs(pos[i + 1] - pos[i]);
+    // }
 
-    cout << floor((candy - dist) / (n * 1.0L));
+    // cout << floor((candy - dist) / (n * 1.0L));
+
+    cout << isValid(450, pos) << endl;
 
     return 0;
 }
