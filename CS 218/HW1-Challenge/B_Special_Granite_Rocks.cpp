5781eadbe08734d6c882c803bffd52444c67d7de
brentnicks <48264066+brentnicks@users.noreply.github.com>
2025-04-06 01:46:59 -0700

    CS218HW1

--- CS 218/HW1-Challenge/B_Special_Granite_Rocks.cpp
+++ CS 218/HW1-Challenge/B_Special_Granite_Rocks.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -23,27 +24,27 @@ using namespace std;
 //     }
 // }
 
-int quick_select(vector<int>& list, int l, int r, int target) {
+int quick_select(vector<int>& list, map<int, int>& mp, int l, int r, int target) {
     int pivot = r;
     int j = l;
     for (int i = l; i < r; i++) {
-        cout << "? " << i + 1 << " " << pivot + 1 << endl;
+        cout << "? " << list[i] << " " << list[pivot] << endl;
         cout.flush();
         string input;
         cin >> input;
         if (input == "<") {
-        // if (list[i] <= list[pivot]) {
             swap(list[i], list[j]);
             j++;
         }
     }
     swap(list[j], list[pivot]);
+    mp[j] = list[j];
     if (j == target) {
         return list[j];
     } else if (j > target) {
-        return quick_select(list, l, j - 1, target);
+        return quick_select(list, mp, l, j - 1, target);
     } else if (j < target) {
-        return quick_select(list, j + 1, r, target);
+        return quick_select(list, mp, j + 1, r, target);
     }
     else return 0;
 }
@@ -58,22 +59,33 @@ int main() {
         int n;
         cin >> n;
         vector<int> list(n);
+        map<int, int> mp;
+
         for (int j = 0; j < n; j++) {
             list[j] = j + 1;
         }
         vector<int> ans;
         for (int j = 1; j < n; j *= 2) {
-            ans.push_back(quick_select(list, 0, n - 1, j - 1));
+            if (mp.find(j - 1) != mp.end()) {
+                ans.push_back(mp[j - 1]);
+                continue;
+            } else {
+                int l = 0, r = n - 1;
+                for (auto it : mp) {
+                    if (it.first < j - 1) {
+                        l = it.first;
+                    } else if (it.first > j - 1) {
+                        r = it.first;
+                    }
+                }
+                ans.push_back(quick_select(list, mp, l, r, j - 1));
+            }
         }
         cout << "! ";
         for (int j = 0; j < ans.size() - 1; j++) {
             cout << ans[j] << " ";
         }
         cout << ans[ans.size() - 1] << endl;
-        for (int i = 0; i < list.size(); i++) {
-            cout << list[i] << " ";
-        }
-        cout << endl;
     }
 
     return 0;
